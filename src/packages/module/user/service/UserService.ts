aed8cd5112876c82b8af204b759e49da86b680ce
Dmitry Atapin <dmitry.atapin.83@gmail.com>
2025-02-24 16:40:02 +0300

    REST service works

--- src/packages/module/user/service/UserService.ts
+++ src/packages/module/user/service/UserService.ts
@@ -12,18 +12,29 @@ export class UserService extends LoggerWrapper {
     declare key: number; 
 
     public createInfo(infoData: Info): number {
+        if (this.key === undefined) {
+            this.key = 0;
+        }
+        if (this.infos === undefined) {
+            this.infos = new Map();
+        }
+
         this.key += 1;
+
         this.infos.set(this.key, infoData);
         return this.key;
     }
 
     public getInfos(): InfoList {
         let key: number = 0;
-        let resultInfos: InfoList;
+        let resultInfos: InfoList = {
+            list: []
+        };
         while (key <= this.key) {
             var info: Info;
             info = this.infos.get(key);
             resultInfos.list.push(info);
+            key++;
         }
         
         return resultInfos;
